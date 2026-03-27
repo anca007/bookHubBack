874fb5778fa3384a51804bc5e1d35a35c95cd372
stropee <stropee@campus-eni.fr>
2026-03-27 10:34:30 +0100

    install starter

---
+++ src/main/java/eni/ecole/bookhubback/BookHubBackApplication.java
@@ -0,0 +1,13 @@
+package eni.ecole.bookhubback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookHubBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookHubBackApplication.class, args);
+    }
+
+}
